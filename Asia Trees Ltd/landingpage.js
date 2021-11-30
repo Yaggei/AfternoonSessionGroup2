2f68eace702c1509f66184c3cbe205b618bb8706
Yaggei <89574929+Yaggei@users.noreply.github.com>
2021-11-30 09:38:53 +0000

    Add files via upload

---
+++ Asia Trees Ltd/landingpage.js
@@ -0,0 +1,36 @@
+function daterefresh(){
+    let seconds = 1000
+    let timeout = setTimeout("displaydate()", seconds)
+}
+function displaydate(){
+    let newdate = new Date()  
+    document.getElementById("yagi").innerHTML = newdate
+    daterefresh()
+
+}
+
+axios.get('https://tbhpwebdevapi.azurewebsites.net/api/Bonsai')                                             
+
+.then(res => {
+    console.log(res.data); 
+    document.getElementById("title").innerHTML = res.data[0].productTitle; 
+    document.getElementById("price").innerHTML = "Price: N" + res.data[0].productPrice;
+    document.getElementById("description").innerHTML = res.data[0].productDescription;
+    document.getElementById("image").src = "https://project1-bonsai-jacobtimms.vercel.app/" + res.data[0].productImage;
+
+
+    document.getElementById("title2").innerHTML = res.data[1].productTitle; 
+    document.getElementById("price2").innerHTML = "Price: N" + res.data[1].productPrice;
+    document.getElementById("description2").innerHTML = res.data[1].productDescription;
+    document.getElementById("image2").src = "https://project1-bonsai-jacobtimms.vercel.app/" + res.data[1].productImage;
+
+
+    document.getElementById("title3").innerHTML = res.data[2].productTitle; 
+    document.getElementById("price3").innerHTML = "Price: N" + res.data[2].productPrice;
+    document.getElementById("description3").innerHTML = res.data[2].productDescription;
+    document.getElementById("image3").src = "https://project1-bonsai-jacobtimms.vercel.app/" + res.data[2].productImage;
+
+})
+.then(err => console.log(err))
+
+
